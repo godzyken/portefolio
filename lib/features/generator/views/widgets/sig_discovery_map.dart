9ec524c69a174aa55312c75797d1a8e76bccfa68
godzyken <isgodzy@gmail.com>
2025-09-17 23:52:27 +0200

    Setup GitHub Actions - deploy

--- lib/features/generator/views/widgets/sig_discovery_map.dart
+++ lib/features/generator/views/widgets/sig_discovery_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:web/web.dart' as web;
 
 import '../../../../core/provider/providers.dart';
 
@@ -16,9 +17,24 @@ class _SigDiscoveryMapState extends ConsumerState<SigDiscoveryMap>
     with SingleTickerProviderStateMixin {
   late final AnimationController _pulseController;
 
+  // Clé injectée avec --dart-define
+  static const mapsApiKey = String.fromEnvironment('MAPS_API_KEY');
+
+  void loadGoogleMaps(String apiKey) {
+    // Injection dynamique du script Google Maps
+    final script = web.HTMLScriptElement();
+    script.src =
+        'https://maps.googleapis.com/maps/api/js?key=$apiKey&libraries=places';
+    script.async = true;
+
+    web.document.head!.append(script);
+  }
+
   @override
   void initState() {
     super.initState();
+
+    loadGoogleMaps(mapsApiKey);
 
     _pulseController = AnimationController(
       vsync: this,
--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,13 @@ import 'package:portefolio/core/affichage/grid_config_provider.dart';
 import 'package:portefolio/core/provider/providers.dart';
 import 'package:portefolio/features/home/views/screens/splash_screen.dart';
 import 'package:portefolio/features/parametres/themes/services/theme_repository.dart';
-import 'package:web/web.dart' as web;
 
 import 'core/routes/router.dart';
 import 'features/generator/views/widgets/responsive_scope.dart';
 import 'features/parametres/themes/controller/theme_controller.dart';
 
-// Clé injectée avec --dart-define
-const mapsApiKey = String.fromEnvironment('MAPS_API_KEY');
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Injection dynamique du script Google Maps
-  final script = web.HTMLScriptElement();
-  script.src =
-      'https://maps.googleapis.com/maps/api/js?key=$mapsApiKey&libraries=places';
-  script.async = true;
-
-  web.document.head!.append(script);
-
   final repo = ThemeRepository();
   final initial = await repo.loadTheme();
 
