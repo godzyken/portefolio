04a3baa89576df9dc8a8b02a199210fd4cedbed5
godzyken <isgodzy@gmail.com>
2026-02-21 20:36:56 +0100

    images en vrac centralisee

---
+++ lib/resources/images.dart
@@ -0,0 +1,25 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String agile = 'assets/images/agile.png';
+  static const String conseillerServiceClient =
+      'assets/images/conseiller_service_client.png';
+  static const String continentalHvac = 'assets/images/continental_hvac.jpg';
+  static const String etricksBmx = 'assets/images/etricks_bmx.png';
+  static const String flutterMascotte = 'assets/images/flutter_mascotte.png';
+  static const String gettyImages = 'assets/images/getty_images.jpg';
+  static const String livreurAmazon = 'assets/images/livreur_amazon.png';
+  static const String mePortrait2 = 'assets/images/me_portrait_2.webp';
+  static const String persDoAm = 'assets/images/pers_do_am.png';
+  static const String pieceMoto = 'assets/images/piece_moto.jpg';
+  static const String prestashop = 'assets/images/prestashop.jpg';
+  static const String tmeLivreur = 'assets/images/tme_livreur.png';
+  static const String transformationDigitale =
+      'assets/images/transformation_digitale.jpg';
+  static const String vignetteClipArticles1 =
+      'assets/images/vignette_clip_articles_1.jpg';
+  static const String vignetteClipArtiste =
+      'assets/images/vignette_clip_artiste.jpg';
+}
