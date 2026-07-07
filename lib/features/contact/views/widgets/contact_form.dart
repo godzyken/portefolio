6b35d336cedf8ebdcb0d6df35c4c014b9c85e4d8
Dore Emryck <33176898+godzyken@users.noreply.github.com>
2026-07-07 22:56:17 +0200

    Update contact_form.dart

--- lib/features/contact/views/widgets/contact_form.dart
+++ lib/features/contact/views/widgets/contact_form.dart
@@ -1,4 +1,4 @@
-mimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portefolio/core/ui/widgets/responsive_text.dart';
 
